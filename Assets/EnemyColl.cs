07963c1c19ff13579ad85ea26113e8039d0dac43
SIMBA\Chris <Christian.Mathuram@hotmail.com>
2018-12-17 18:58:36 +0530

    push-1

---
+++ Assets/EnemyColl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyColl : MonoBehaviour {
+
+    public float Starthealth = 100;
+    private float health;
+
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+    void Start()
+    {
+        health = Starthealth;    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            //  collision.gameObject.SendMessage("ApplyDamage", 10);
+            Debug.Log("Collision detected --> Tree");
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            //  collision.gameObject.SendMessage("ApplyDamage", 10);
+            health -= 10;
+            Debug.Log("Collision detected --> Player");
+            healthBar.fillAmount = health / Starthealth;
+           
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    private Animator anim;
+    void Start()
+    {
+          anim = GetComponent<Animator>();
+    }
+    void Update () {
+        int PlayerHealth = 10;
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime;
+        var x = Input.GetAxisRaw("Horizontal");
+        Debug.Log("From here -1");
+        Debug.Log(x);
+        if (x < 0)
+        {
+            anim.SetTrigger("Trig");
+            GetComponent<Transform>().eulerAngles = new Vector3(0,-180,0);
+        }
+
+        if (x > 0)
+        {
+            anim.SetTrigger("Trig");
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            
+        }
+
+        if (PlayerHealth == 0)
+        {
+            movement = new Vector3(0, 0, 0.0f);
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+    
+    }
+
+}
