60af481377c892459be4fccddc3172e684eb0a31
SIMBA\Chris <Christian.Mathuram@hotmail.com>
2019-01-06 21:58:34 +0530

    Mouse Control (Combat)

--- Assets/EnemyColl.cs
+++ Assets/EnemyColl.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class EnemyColl : MonoBehaviour {
 
+    Mouse mousectrl;
+
     public float Starthealth = 100;
-    private float health;
+    public float health;
+    
 
     [Header("Unity Stuff")]
     public Image healthBar;
-
+    
     void Start()
     {
         health = Starthealth;    
@@ -17,19 +20,17 @@ public class EnemyColl : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Tree")
-        {
-            //  collision.gameObject.SendMessage("ApplyDamage", 10);
-            Debug.Log("Collision detected --> Tree");
-        }
+        //if (collision.gameObject.tag == "Tree")
+        //{
+        //    //  collision.gameObject.SendMessage("ApplyDamage", 10);
+        //    Debug.Log("Collision detected --> Tree");
+        //}
+
         if (collision.gameObject.tag == "Player")
         {
-            //  collision.gameObject.SendMessage("ApplyDamage", 10);
-            health -= 10;
-            Debug.Log("Collision detected --> Player");
-            healthBar.fillAmount = health / Starthealth;
-           
+            OnMouseDown();
         }
+
         Debug.Log(health);
         if(health <= 0)
         {
@@ -37,4 +38,21 @@ public class EnemyColl : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+    public void OnMouseDown()
+    {
+        Debug.Log("Mouse Click");
+
+        //  collision.gameObject.SendMessage("ApplyDamage", 10);
+        health -= 10;
+        Debug.Log("Collision detected --> Player");
+        healthBar.fillAmount = health / Starthealth;
+
+    }
+
+}
+
+public class Mouse
+{
+    EnemyColl EC;
+    
 }
