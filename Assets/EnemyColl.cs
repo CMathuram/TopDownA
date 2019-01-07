ef90a797cf25f5062b2acb3fefac774543c41407
SIMBA\Chris <Christian.Mathuram@hotmail.com>
2019-01-07 14:22:06 +0530

    Mouse Control Patch update

--- Assets/EnemyColl.cs
+++ Assets/EnemyColl.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class EnemyColl : MonoBehaviour {
-
-    Mouse mousectrl;
+public class EnemyColl : MonoBehaviour
+{
 
     public float Starthealth = 100;
     public float health;
-    
+    public int attack = 0;
 
     [Header("Unity Stuff")]
     public Image healthBar;
-    
+
     void Start()
     {
-        health = Starthealth;    
+        health = Starthealth;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -28,31 +27,35 @@ public class EnemyColl : MonoBehaviour {
 
         if (collision.gameObject.tag == "Player")
         {
-            OnMouseDown();
+            //mousectrl.OnMouseDown();
+            //OnMouseDown();
+            attack = 1;
         }
 
-        Debug.Log(health);
-        if(health <= 0)
-        {
-            //Death animation here
-            Destroy(gameObject);
-        }
+       
     }
+
+
     public void OnMouseDown()
     {
-        Debug.Log("Mouse Click");
+        if (attack == 1)
+        {
+            Debug.Log("Mouse Click");
 
-        //  collision.gameObject.SendMessage("ApplyDamage", 10);
-        health -= 10;
-        Debug.Log("Collision detected --> Player");
-        healthBar.fillAmount = health / Starthealth;
+            //  collision.gameObject.SendMessage("ApplyDamage", 10);
+            health -= 10;
+            Debug.Log("Collision detected --> Player");
+            healthBar.fillAmount = health / Starthealth;
 
+            //Enemy's Health
+
+            Debug.Log(health);
+            if (health <= 0)
+            {
+                //Death animation here
+                Destroy(gameObject);
+            }
+        }
     }
 
-}
-
-public class Mouse
-{
-    EnemyColl EC;
-    
 }
